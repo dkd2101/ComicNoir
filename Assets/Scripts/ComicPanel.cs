e4b00e0f6be905d9e6ea165d6bcfcf472b6fca14
Jake Pine <jpinedev@gmail.com>
2022-11-11 20:57:10 -0500

    started comic strip ui

---
+++ Assets/Scripts/ComicPanel.cs
@@ -0,0 +1,51 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct ComicPanel
+{
+    public ComicPanelType Type;
+
+    public ComicPanelImageFrame Frame;
+    public Sprite Image;
+    public List<ComicText> Texts; 
+    
+    public string Monologue;
+}
+
+public enum ComicPanelType
+{
+    Image,
+    Monologue
+}
+
+public enum ComicPanelImageFrame
+{
+    Rect,
+    ShearedRect,
+    Trapezoid
+}
+
+public struct ComicText
+{
+    // TODO: add different bubble types later
+    public ComicTextType Type;
+    public string Text;
+}
+
+public enum ComicTextType
+{
+    Monologue,
+    McDialogue, // Main Character Dialogue
+    NpcDialogue // Non-Player Character Dialogue
+}
+
+/*
+ TODO: add different bubble types later
+public enum DialogueType
+{
+    Normal,
+    Shocked,
+    
+}
+*/
---
+++ Assets/Scripts/ComicStrip.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ComicStrip", menuName = "Comic/Comic Strip", order = 0)]
+public class ComicStrip : ScriptableObject
+{
+    
+}
