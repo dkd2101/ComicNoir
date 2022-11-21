dd1609532eb060ac874822ff717cf6dc9c78c0d0
Dylan Dinio <DDinio2101@gmail.com>
2022-11-21 13:15:24 -0500

    Created Scriptable Objects for Evidence and Conditions for triggering events in the evidence board

---
+++ Assets/Scripts/ScriptableObjects/Conditions.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conditions : ScriptableObject
+{
+    [SerializeField]
+    List<EvidenceSO> necesssarry_connections;
+
+    //[SerializeField]
+    //ComicStrip conclusions;
+}
---
+++ Assets/Scripts/ScriptableObjects/EvidenceSO.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu]
+public class EvidenceSO : ScriptableObject
+{
+    [SerializeField]
+    Sprite object_sprite;
+
+    [SerializeField]
+    string evidence_name;
+
+    [SerializeField]
+    string description;
+
+    //this represented the current "connections" the player has made to this evidence
+    // may need a add function for connected_evidence
+    [SerializeField]
+    List<EvidenceSO> connected_evidence;
+
+    [SerializeField]
+    List<Conditions> conclusions;
+}
