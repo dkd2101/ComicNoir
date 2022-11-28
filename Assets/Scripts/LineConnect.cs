4c3d5dc645299bdf22a3f334bace0276aef991ce
Jacob Pine <jpinedev@gmail.com>
2022-11-28 13:24:50 -0500

    initial connection framework

--- Assets/Scripts/LineConnect.cs
+++ Assets/Scripts/LineConnect.cs
@@ -1,19 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class LineConnect : MonoBehaviour
 {
-    public GameObject pointOne;
-    public GameObject pointTwo;
-
-    private List<GameObject> connectedObjects;
+    public Transform startingPoint;
 
     private LineRenderer drawer;
+
+    public static UnityEvent<Transform> selectItem;
+
+    [SerializeField] private LineRenderer connectionPreview;
+
+    [SerializeField] private LineRendererController _connectionPrefab;
+    
+    private Vector3 MousePosition {
+        get {
+            var mPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mPos.z = 0;
+            return mPos;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
+        selectItem.AddListener(SelectItem);
+
+        if (connectionPreview == null) connectionPreview = GetComponent<LineRenderer>();
+        
         drawer = this.gameObject.AddComponent<LineRenderer>();
         
         drawer.SetWidth(0.05F, 0.05F);
@@ -21,18 +37,42 @@ public class LineConnect : MonoBehaviour
         drawer.SetVertexCount(2);
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    private void Update()
     {
-        for(int i = 0; i < connectedObjects.Count; i++)
-        {
-            if(i+1 < connectedObjects.Count)
-            {
-                pointOne = connectedObjects[i];
-                pointTwo = connectedObjects[i + 1];
-                drawer.SetPosition(0, pointOne.transform.position);
-                drawer.SetPosition(1, pointTwo.transform.position);
-            }
-        }
+        if (startingPoint == null) return;
+        
+        connectionPreview.SetPosition(1, MousePosition);
     }
+
+    private void SelectItem(Transform item)
+    {
+        if (startingPoint == null)
+        {
+            startingPoint = item;
+            connectionPreview.SetPosition(0, startingPoint.position);
+            connectionPreview.SetPosition(1, MousePosition);
+            connectionPreview.enabled = true;
+            return;
+        }
+
+        if (startingPoint == item)
+        {
+            startingPoint = null;
+            connectionPreview.enabled = false;
+            return;
+        }
+
+        MakeConnection(startingPoint, item);
+        
+        startingPoint = null;
+    }
+
+    private void MakeConnection(Transform item1, Transform item2)
+    {
+        // Connect the evidence pieces, create a new LineRenderer and hand off to one of the evidence pieces
+        // either Have those evidence pieces update the positions, or create
+        var lrController = Instantiate(_connectionPrefab, transform);
+        lrController.Initialize(item1, item2);
+    }
+    
 }
